8c74fa3efe79a4ae88cbc5c63d6a2639628a753a
timothy adigun <adigun.timothy@gmail.com>
2026-06-29 23:17:56 +0100

    updating delete option

---
+++ makerkt.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+# Date: 29/06/2026
+# Description: A script for programming Racket
+# Author: omitida
+#
+
+filename=
+
+function usage() {
+    echo "Usage: $0 <option> <filename>"
+    echo "  "
+
+    echo "Available options:"
+    echo "------------------"
+    echo "-c: compile the Racket file."
+    echo "-d: delete the compiled executable."
+    echo "-h: show this help message."
+    echo "-o: generate standalone Racket file."
+    echo "-r: run the compiled executable."
+    exit 1
+}
+
+function check_file() {
+    filename="${1}"
+    file_extension="${filename##*.}"
+    file_basename="${filename%.*}"
+    [ "${file_extension}" != "rkt" ] && { filename="${file_basename^}.rkt"; }
+}
+
+RACKET="module Main
+
+main : IO ()
+main = putStrLn \"Hello, World!\"
+"
+
+if [ "${#}" -ne 2 ]; then
+    usage
+fi
+
+optionstring="c:d:o:r:h"
+while getopts "${optionstring}" opt; do
+    case $opt in
+        c)
+            filename="${OPTARG}"
+            echo "Compiling $filename..."
+            check_file "${filename}"
+
+            ;;
+
+        d)
+            filename="${OPTARG}"
+            echo "Deleting compiled executable..."
+
+            while read -p -r "Are you sure you want to delete ${filename}? (y/n): " yn; do
+                case "${yn}" in
+                    [Yy]* ) rm -f "${filename}"; break;;
+                    [Nn]* ) break;;
+                    * ) echo "${filename} Not deleted..."
+                ;;
+            esac
+        done
+        ;;
+        r)
+            filename="${OPTARG}"
+            echo "Running compiled executable..."
+            check_file "${filename}"
+
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            echo "Invalid option: -${OPTARG}" >&2
+            usage
+            ;;
+    esac
+done
