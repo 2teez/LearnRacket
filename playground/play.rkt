967ba9cab696905f56e79b4eb338676334bdb058
timothy adigun <adigun.timothy@gmail.com>
2026-06-30 11:25:38 +0100

    playing with racket

---
+++ playground/play.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define a-number 123)
+(define b-number a-number)
+(displayln (list a-number 'eq b-number (eq? a-number b-number)))
+(printf "~a == ~a is ~a\n" a-number b-number (equal? a-number b-number))
+;;(list (car a-number) (cdr b-number)) ; this would work car/cdr expect a pair
+
+(displayln ((lambda (x) (+ x 1)) 1)) ; anonymous function/procedure
+
+(define add-1-to (lambda (x) (+ x 1)))
+
+(writeln (add-1-to 3))
+
