6a119f76c349a8743acb7efc49062592b3b3c734
timothy adigun <adigun.timothy@gmail.com>
2026-06-30 12:52:02 +0100

    using procedure and recusive in racket

--- playground/recursive.rkt
+++ playground/recursive.rkt
@@ -1,3 +1,15 @@
 #lang racket
 
 (displayln "Start Here!")
+
+(define (fact x)
+  (if (<= x 1) 1 (* x (fact (- x 1)))))
+
+(displayln (fact 5)) ;; > 120
+
+(define (looper lst acc)
+  (if (empty? lst)
+      acc
+      (looper (rest lst)
+              (cons (fact (first lst)) acc))))
+(println (looper (range 100 0 -3) '())) ;; > '()
