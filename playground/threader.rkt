41e7e491aee718d8e5265b4f02d29a675cc0942f
timothy adigun <adigun.timothy@gmail.com>
2026-06-30 19:20:58 +0100

    using thred in racket

--- playground/threader.rkt
+++ playground/threader.rkt
@@ -1,4 +1,16 @@
 #lang racket
 
 (displayln "Start, Here!")
+(define (detailed-fact n)
+  (begin
+    (displayln (format "Calculating factorial of ~a.." n))
+    (cond ((= n 0) 1)
+          (else (* n (detailed-fact (- n 1)))))))
+(begin (detailed-fact 5)
+  (detailed-fact 23))
 
+;; using thread
+(writeln "Using threading macro in racket.")
+(begin
+  (thread (lambda () (detailed-fact 5)))
+  (thread (lambda () (detailed-fact 23))))
