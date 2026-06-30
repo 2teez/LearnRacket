7ecadfeca7a3540d2815fd76ba0d9219100ccd21
timothy adigun <adigun.timothy@gmail.com>
2026-06-30 15:07:22 +0100

    using iter in racket

--- playground/recursive.rkt
+++ playground/recursive.rkt
@@ -5,7 +5,12 @@
 (define (fact x)
   (if (<= x 1) 1 (* x (fact (- x 1)))))
 
+(define (fact-iter x acc)
+  (cond ((<= x 1) acc)
+        (else (fact-iter (- x 1) (* x acc)))))
+
 (displayln (fact 5)) ;; > 120
+(displayln (fact-iter 5 1)) ;; > 120
 
 (define (looper lst acc)
   (if (empty? lst)
@@ -14,3 +19,13 @@
               (cons (fact (first lst)) acc))))
 (println (looper (range 100 0 -3) '())) ;;
 (println (looper (reverse (range 100 0 -3)) '())) ;;
+
+;; using fact-iter to avoid stack overflow
+;; using car/cdr instead of first/rest
+(define (looper-iter lst acc)
+  (if (empty? lst)
+      acc
+      (looper-iter (cdr lst)
+                   (cons (fact-iter (car lst) 1) acc))))
+(println (looper-iter (range 100 0 -3) '())) ;;
+(println (looper-iter (reverse (range 100 0 -3)) '())) ;;
